3d0032b63d0a6600c90d3b5a79199adba345e315
sagie gur ari <sagiegurari@gmail.com>
2018-06-14 21:06:32 +0300

    cleanups

--- project/config/karma.conf.js
+++ project/config/karma.conf.js
@@ -4,52 +4,13 @@ module.exports = function (config) {
     'use strict';
 
     var mainJSFile = require('../../bower.json').main;
+    var commons = require('js-project-commons');
 
-    var karmaConfig = {
-        basePath: '../../',
-        frameworks: [
-            'mocha',
-            'sinon-chai'
-        ],
+    commons.tools.karma(config, {
         files: [
             'test/helpers/**/*.js',
             mainJSFile,
             'test/spec/**/*.js'
-        ],
-        port: 8080,
-        logLevel: config.LOG_INFO,
-        autoWatch: false,
-        browsers: [
-            'PhantomJS'
-        ],
-        singleRun: false,
-        reporters: [
-            'progress',
-            'coverage'
-        ],
-        preprocessors: {},
-        coverageReporter: {
-            dir: 'target/coverage/report',
-            reporters: [
-                {
-                    type: 'lcov',
-                    subdir: '.'
-                }
-            ],
-            check: {
-                global: {
-                    statements: 100,
-                    functions: 100,
-                    lines: 100,
-                    branches: 100
-                }
-            }
-        }
-    };
-
-    karmaConfig.preprocessors[mainJSFile] = [
-        'coverage'
-    ];
-
-    config.set(karmaConfig);
+        ]
+    }, mainJSFile);
 };
